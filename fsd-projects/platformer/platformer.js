e330b0ee26ec8306783c2b4cdc8224c5d4fc858f
fhozo <159402945+fhozo@users.noreply.github.com>
2025-09-10 16:51:35 +0000

    new file:   .vscode/settings.json 	new file:   assets/IMG_4025.JPG 	new file:   assets/IMG_4026.JPG 	new file:   assets/IMG_4027.JPG 	new file:   assets/IMG_4028.JPG 	new file:   assets/IMG_4029.JPG 	modified:   fsd-projects/platformer/platformer.js 	new file:   images.html 	modified:   index.html 	modified:   portfolio.html

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,23 +27,37 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+     //toggleGrid();
 
 
     // TODO 2 - Create Platforms
+createPlatform(1000, 0, 20, 520);
 
-
-
-
+createPlatform(50, 600, 160, 10, "red");
+createPlatform(400, 490, 50, 10, "red");
+createPlatform(560, 490, 50, 10, "red");
+createPlatform(760, 390, 20, 10, "red");
+createPlatform(500, 0, 20, 350);
+createPlatform(1200, 0, 20, 750);
+createPlatform(995, 650, 20, 10, "red");
+createPlatform(1180, 550, 20, 10, "red");
+createPlatform(1020, 430, 20, 10, "red");
+createPlatform(1180, 300, 20, 10, "red");
+createPlatform(1020, 200, 20, 10, "red");
+createPlatform(520, 300, 60, 10, "red");
     // TODO 3 - Create Collectables
-
-
-
-    
-    // TODO 4 - Create Cannons
+createCollectable("steve", 1150, 30);
+createCollectable("diamond", 520, 170, 0.5, 0.7);
 
 
     
+    // TODO 4 - Create Cannons 500, 2000, 3500, 0.0001
+createCannon("top", 200, 500);
+createCannon("right", 300, 2000);
+createCannon("right", 600, 3500);
+createCannon("right", 800, 0.0001);
+createCannon("bottom", 1040, 1500);
+createCannon("bottom", 480, 2000);
     
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
